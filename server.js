c84d2d0bfac53a6b4e537d90f2745ebdefbc187a
DaRealXrus <roblox0936coolguy@gmail.com>
2026-02-03 20:08:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Serve the HTML files
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/overlay', (req, res) => res.sendFile(path.join(__dirname, 'overlay.html')));
 
 io.on('connection', (socket) => {
-    // When someone joins, put them in a "room" based on the streamer's name
     socket.on('join', (room) => {
         socket.join(room);
-        console.log(`User joined room: ${room}`);
     });
 
-    // When a viewer sends a message
     socket.on('tts-msg', (data) => {
-        // Send it ONLY to the streamer's room
-        io.to(data.streamer).emit('play-tts', data.message);
+        // data now contains: streamer, username, and message
+        io.to(data.streamer).emit('play-tts', { 
+            username: data.username, 
+            message: data.message 
+        });
     });
 });
 
