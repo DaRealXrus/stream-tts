a0b4842cd1dae616dab86882924366d288b114ad
DaRealXrus <roblox0936coolguy@gmail.com>
2026-02-03 20:49:12 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ const path = require('path');
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/overlay', (req, res) => res.sendFile(path.join(__dirname, 'overlay.html')));
 
+// THE BLACKLIST
+const bannedWords = ["nigger", "faggot"]; 
+
 io.on('connection', (socket) => {
     socket.on('join', (room) => {
         socket.join(room);
     });
 
     socket.on('tts-msg', (data) => {
-        // data now contains: streamer, username, and message
+        const msgLower = data.message.toLowerCase();
+        
+        // CHECK IF MESSAGE HAS BANNED WORDS
+        const containsBanned = bannedWords.some(word => msgLower.includes(word));
+        
+        if (containsBanned) {
+            console.log(`Blocked a message from ${data.username}: ${data.message}`);
+            return; // STOP HERE. Don't send it to the overlay.
+        }
+
         io.to(data.streamer).emit('play-tts', { 
             username: data.username, 
             message: data.message 
