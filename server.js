d38d046ffcda6f52386a0a53bfb1a996108c31b7
DaRealXrus <roblox0936coolguy@gmail.com>
2026-02-03 21:04:04 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,23 +7,22 @@ const path = require('path');
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/overlay', (req, res) => res.sendFile(path.join(__dirname, 'overlay.html')));
 
-// BLOCK LIST - Messages with these will be deleted instantly
 const banned = ["nigger", "faggot"];
 
 io.on('connection', (socket) => {
-    socket.on('join', (room) => socket.join(room));
+    socket.on('join', (room) => {
+        socket.join(room);
+        console.log(`Streamer joined: ${room}`);
+    });
 
     socket.on('tts-msg', (data) => {
         const cleanMsg = data.message.toLowerCase();
-        // If it has a banned word, ignore it entirely
         if (banned.some(word => cleanMsg.includes(word))) return;
 
-        io.to(data.streamer).emit('play-tts', { 
-            username: data.username, 
-            message: data.message 
-        });
+        console.log(`Message received for ${data.streamer}: [${data.username}] ${data.message}`);
+        io.to(data.streamer).emit('play-tts', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server Live'));
+http.listen(PORT, () => console.log('Server is running!'));
